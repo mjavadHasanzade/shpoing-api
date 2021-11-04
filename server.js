7bf4f7c39006820840b30b175ff5bd919ef7fe2a
Mjavad Hasanzade <hjavad522@gmail.com>
2021-11-05 01:21:55 +0330

    fix: fix issue with database creation

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync({ force: true }).then(async () => {
-    for (let i = 1; i <= 15; i++) {
-        const product = {
-            name: `product ${i}`,
-            description: `description ${i}`
-        }
-        await Product.create(product);
-    }
+sequelize.sync({ force: false }).then(async () => {
+    // for (let i = 1; i <= 15; i++) {
+    //     const product = {
+    //         name: `product ${i}`,
+    //         description: `description ${i}`
+    //     }
+    //     await Product.create(product);
+    // }
 });
 
 app.use('/product', routes);
