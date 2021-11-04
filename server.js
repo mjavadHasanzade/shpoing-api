e077e9550306ca19596fff6976c32968b32d9e8a
Mjavad Hasanzade <hjavad522@gmail.com>
2021-11-04 19:58:10 +0330

    feat: sync database

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const sequelize = require('./database');
+const Product = require('./models/product');
 const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
+sequelize.sync({ force: true }).then(async () => {
+    for (let i = 1; i <= 15; i++) {
+        const product = {
+            name: `product ${i}`,
+            description: `description ${i}`
+        }
+        await Product.create(product);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`app is listening to port ${PORT}`);
