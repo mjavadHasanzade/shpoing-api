cd81e5f970436b4e89651ce796bac9296ed7200a
mjavad_hasanzade <hjavad522@gmail.com>
2021-11-12 19:57:58 +0330

    feat: add i18next messages to jo validator

--- validators/product.js
+++ validators/product.js
@@ -3,16 +3,25 @@ const Joi = require('joi');
 const productValidator = (data) => {
 
     const schema = Joi.object({
-        name: Joi.string().required(),
-        description: Joi.string().required(),
-        price: Joi.number().min(0).required(),
-        oldPrice: Joi.number().min(0),
+        name: Joi.string().required().messages({
+            "any.required": data.t('product_name_null')
+        }),
+        description: Joi.string().required().messages({
+            "any.required": data.t('product_description_null')
+        }),
+        price: Joi.number().min(0).required().messages({
+            "any.required": data.t('product_price_null'),
+            "number.min": `${data.t('product_price_size')} {#limit} `,
+        }),
+        oldPrice: Joi.number().min(0).messages({
+            "number.min": `${data.t('product_price_size')} {#limit} `,
+        }),
         category: Joi.string(),
         properties: Joi.string(),
         quantity: Joi.number(),
     });
 
-    return schema.validate(data);
+    return schema.validate(data.body);
 
 }
 
