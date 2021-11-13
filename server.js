075d191551c8057bbdb23c0fb5242f454fe3947b
mjavad_hasanzade <hjavad522@gmail.com>
2021-11-13 18:03:33 +0330

    feat: add user functionalities to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sequelize = require('./database');
 const Product = require('./models/product');
 const routes = require('./routes/product');
+const userRroutes = require('./routes/users');
 var bodyParser = require('body-parser')
 const errorHandler = require('./utils/errorHandler');
 const app = express();
@@ -19,16 +20,16 @@ i18next.use(i18Backend).use(i18Middleware.LanguageDetector).init({
     }
 })
 
-sequelize.sync({ force: true }).then(async () => {
-    for (let i = 1; i <= 15; i++) {
-        const product = {
-            name: `product ${i}`,
-            description: `description ${i}`,
-            price: Math.floor(Math.random() * 1000000),
-            quantity: Math.floor(Math.random() * 100)
-        }
-        await Product.create(product);
-    }
+sequelize.sync({ force: false }).then(async () => {
+    // for (let i = 1; i <= 15; i++) {
+    //     const product = {
+    //         name: `product ${i}`,
+    //         description: `description ${i}`,
+    //         price: Math.floor(Math.random() * 1000000),
+    //         quantity: Math.floor(Math.random() * 100)
+    //     }
+    //     await Product.create(product);
+    // }
 });
 
 app.use(bodyParser.urlencoded({
@@ -40,6 +41,7 @@ app.use(express.json());
 app.use(i18Middleware.handle(i18next));
 
 app.use('/product', routes);
+app.use('/users', userRroutes);
 
 
 app.use(errorHandler);
