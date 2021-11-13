847a37993c74351beb679a9ec623b0a9e80178c5
mjavad_hasanzade <hjavad522@gmail.com>
2021-11-13 17:59:22 +0330

    feat: add user routes

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const { getAll, getOne, createOne, editOne, deleteOne } = require('../controllers/user');
+const router = require('express').Router();
+const pagination = require('../middlewars/pagination');
+
+router.get('/', pagination, getAll);
+router.get('/:id', getOne);
+router.post('/', createOne);
+router.put('/:id', editOne);
+router.delete('/:id', deleteOne);
+
+module.exports = router;
